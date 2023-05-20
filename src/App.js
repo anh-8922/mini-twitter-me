13d1e86ea3540c2d58c096e1abcb4edc9a4a513c
lranatunga <rmlik.ranatunga@gmail.com>
2023-05-20 10:05:11 +0100

    All users added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AllMessages from './Components/allMessage';
+import AllUsers from './Components/allUsers';
 
 function App() {
   return (
     <div className="App">
       <AllMessages/>
+      <AllUsers/>
     </div>
   );
 }
---
+++ src/Components/allUsers.js
@@ -0,0 +1,28 @@
+import useFetchData from "../CustomHooks/useFetchData";
+
+
+export default function AllUsers() {
+  const { data } = useFetchData('https://twitter-express-server.vercel.app/users/list');
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const { users } = data;
+
+  return (
+    <div>
+      {users.map((item, index) => (
+        <div key={index}>
+          <div><img src={item.image}/></div>
+          <div>User Name: {item.username}</div>
+          <div>Email: {item.email}</div>
+          <div>City: {item.city} Country: {item.country}</div>
+          <div>Age: {item.age}</div>
+          <div>Gender: {item.gender}</div>
+          <div>Hobbies: {item.hobbies[0]}, {item.hobbies[1]}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
