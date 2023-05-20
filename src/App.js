af03a13287d85d355a67487b1f204668e318fdc6
lranatunga <rmlik.ranatunga@gmail.com>
2023-05-20 14:24:35 +0100

    adding posts

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import SearchBar from './Components/serachBar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchPage from './Pages/SearchPage';
 import CurrentUser from './Components/currentUser';
+import AddNewMessage from './Components/addNewPost';
 
 function App() {
   return (
     <BrowserRouter>
-      <SearchBar/>
-      {/* <AllMessages/>
-      <AllUsers/> */}
-      <CurrentUser/>
-      <Routes>
+    <AddNewMessage/>
+      {/* <SearchBar/> */}
+      <AllMessages/>
+      {/* <AllUsers/> */}
+      {/* <CurrentUser/> */}
+      {/* <Routes>
         <Route path='/search' element={<SearchPage/>}/>
-      </Routes>
+      </Routes> */}
       
     </BrowserRouter>
   );
