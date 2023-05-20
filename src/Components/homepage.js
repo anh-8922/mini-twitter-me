867a77ffcb8ed9eaf33887849ab82dba58ecc8c9
Anh Chau <anhchau8922@gmail.com>
2023-05-20 15:50:48 +0100

    updates

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -1,13 +1,13 @@
 import AllMessages from "./allMessage";
 import React from "react";
 import "./css/pages.css";
-import AddNewMessage from "./addNewPost";
+
 
 export default function Home() {
     return(
         <section id="display-main">
             
-            <div id="newpost"><AddNewMessage  /></div>
+            
                 
             
             <AllMessages />
--- src/layout.js
+++ src/layout.js
@@ -4,9 +4,16 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import SearchBar from "./Components/serachBar";
-
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
+import AddNewMessage from "./Components/addNewPost";
 export default function Layout() {
-    //const [click, setClick] = useState(false);
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
     return (
         <div id="layout">
             <div id="header">
@@ -26,7 +33,23 @@ export default function Layout() {
                     <Nav defaultActiveKey="/home" className="flex-column" id="nav-item">
                         <Nav.Link href="/home">Home</Nav.Link>
                         <Nav.Link eventKey="link-1" href="user" >People</Nav.Link>
-                        <Nav.Link eventKey="link-2" href="newpost">Add new post</Nav.Link>
+                        <Button  onClick={handleShow} id="add-button">
+                            Add new post
+                        </Button>
+                        <Modal show={show} onHide={handleClose} animation={false} style={{height: "20rem"}}>
+                            <Modal.Header closeButton>
+                            <Modal.Title>Add new post</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body ><AddNewMessage/></Modal.Body>
+                            <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                                Close
+                            </Button>
+                            <Button variant="primary" onClick={handleClose}>
+                                Save Changes
+                            </Button>
+                            </Modal.Footer>
+                        </Modal>
                         
                     </Nav>
                 </nav>
